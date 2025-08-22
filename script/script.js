5dd63a72a26648d007d6505fe8dcd1ed22a302a7
Vips43 <vipulgehlot43@gmail.com>
2025-08-22 11:22:27 +0530

    dede

--- script/script.js
+++ script/script.js
@@ -1,30 +1,37 @@
 const closeBtn = document.getElementById('close'),
-currDate = document.querySelector('.date'),
-addNoteWrapper = document.querySelector('.add-note-wrapper'),
-addNoteBox = document.querySelector('.add-note-box'),
-openBlankNote = document.querySelector('.add-btn'),
-addBtn = document.getElementById('addBtn')
+    currDate = document.querySelector('.date'),
+    addNoteWrapper = document.querySelector('.add-note-wrapper'),
+    addNoteBox = document.querySelector('.add-note-box'),
+    openBlankNote = document.querySelector('.add-btn'),
+    h4 = document.querySelector('.header h4'),
+    addBtn = document.getElementById('addBtn')
 
 let input = document.getElementById('input'),
-title = document.querySelector('.title'),
-desc = document.getElementById('desc')
+    title = document.querySelector('.title'),
+    desc = document.getElementById('desc')
 
-const months = ['January', 'february', 'March',' April',' May', 'June', 'July', 'August', 'September', 'November', 'December']
+const months = ['January', 'february', 'March', ' April', ' May', 'June', 'July', 'August', 'September', 'November', 'December']
 
 const notes = JSON.parse(localStorage.getItem("notes") || "[]");
 
+let isUpdate = false, updateId;
+
 //open for adding task
-openBlankNote.onclick = ()=>{
+openBlankNote.onclick = () => {
     addNoteWrapper.classList.remove('show')
-    setTimeout(() => {
-        addNoteWrapper.style.display = 'flex'
-    }, 800);
+    h4.textContent = "Add a new Note"
+    addBtn.textContent = "Add Note"
+
 }
-closeBtn.onclick = ()=>{
+closeBtn.onclick = () => {
+    isUpdate = false
     addNoteWrapper.classList.add('show')
-    setTimeout(() => {
-        addNoteWrapper.style.display = 'none'
-    }, 800);
+
+    input.value = '';
+    desc.value = '';
+
+    h4.textContent = "Add a new Note"
+    addBtn.textContent = "Add Note"
 }
 //open for adding task ends
 //setting date
@@ -36,22 +43,37 @@ function setDate() {
     return `${month} ${day}, ${year}`
 }
 
-addBtn.onclick = ()=>{
-    const inputValue = input.value,
-    descValue = desc.value;
-    let noteInfo = {
-        title:inputValue,
-        desc:descValue,
-        currDate:setDate()
+addBtn.onclick = () => {
+    const inputValue = input.value.trim(),
+        descValue = desc.value.trim();
+    if (inputValue == '' && descValue == '') {
+        alert('Please write something in notes to add ')
+        return
     }
-    notes.push(noteInfo)
-    console.log(notes);
-    localStorage.setItem('notes', JSON.stringify(notes))
-    addNote()
+    else if (title||desc) {
+        
+        let noteInfo = {
+            title: inputValue,
+            desc: descValue,
+            currDate: setDate()
+        }
+        if(!isUpdate){
+            notes.push(noteInfo)
+        }else{
+            isUpdate = false;
+            notes[updateId] = noteInfo
+        }
+
+
+        localStorage.setItem('notes', JSON.stringify(notes));
+        addNote();
+    }
+
 }
 
 function addNote() {
-    notes.forEach(note => {
+    document.querySelectorAll('.note-wrapper').forEach(note => note.remove())
+    notes.forEach((note, index) => {
         let liTag = `<li class="note-wrapper">
                         <div class="content">
                             <h3 class="title">${note.title}</h3>
@@ -62,17 +84,34 @@ function addNote() {
                             <div class="setting">
                             <i class="fa-solid fa-ellipsis"></i>
                             <ul class="menu">
-                               <li onclick=editNote()><i class="fa-solid fa-pencil"></i> Edit</li>
-                                <li><i class="fa-solid fa-trash"></i> Delete</li>
+                               <li onclick=editNote(${index},'${note.title}','${note.desc}')><i class="fa-solid fa-pencil"></i> Edit</li>
+                                <li onclick=deleteNote(${index})><i class="fa-solid fa-trash"></i> Delete</li>
                             </ul>
                             </div>       
                         </div>
                     </li>`
-        addNoteBox.insertAdjacentHTML("afterend",liTag);
+        addNoteBox.insertAdjacentHTML("afterend", liTag);
     });
 }
 addNote()
 
+function editNote(noteID, title, des) {
+    isUpdate = true;
+    updateId = noteID;
+    openBlankNote.click()
+    h4.textContent = "Update a Note"
+    addBtn.textContent = "Update"
+    //updating text values
+    input.value = title;
+    desc.value = des;
 
+}
+function deleteNote(noteID) {
+    let confirmDel = confirm("Are you sure want to delete this note")
+    if(!confirmDel) return
+    notes.splice(noteID, 1)
+    localStorage.setItem('notes', JSON.stringify(notes));
+    addNote()
+}
 
 
